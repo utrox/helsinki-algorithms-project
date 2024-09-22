ae560244aa540d00c3264215fc9471c52f0438ef
utrox <89227600+utrox@users.noreply.github.com>
2024-09-22 14:51:53 +0300

    Create week3.py

---
+++ docs/weekly_reports/week3.py
@@ -0,0 +1,15 @@
+# Week #3 report
+
+### What have you done this week?
+- Implement and optimize my Miller-Rabin primality test algorithm
+- Completed, but not merged, because finishing writing tests is WIP:
+    - Generating the RSA keys
+    - Encoding in asn.1 format, and storing keys in files
+
+### What has been unclear or problematic? 
+- I'm fine to use existing [asn1 encoding library](https://pypi.org/project/pyasn1/), intead of implementing that myself, right? 
+
+### What next?
+- Public key encryption (Confidentality) with private key decryption
+- Private key encryption (Authenticity) with public key decryption
+- Using both encryptions to get the best of both worlds
